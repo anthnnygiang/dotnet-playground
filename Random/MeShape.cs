ffdc8d048c0d7d23139092d0f39a970ac1868944
Anthony Giang <anthnnygiang@icloud.com>
2024-10-03 21:44:26 +1000

    move to multiple projects

--- Random/MeShape.cs
+++ Random/MeShape.cs
@@ -2,19 +2,19 @@ using System;
 
 namespace dotnet_playground;
 
-public class Shape
+public class MeShape
 {
     public string InfoString;
-    public Shape(string info) => InfoString = info;
+    public MeShape(string info) => InfoString = info;
 
     static void ValueTypeContainingRefType()
     {
         // Create the first Rectangle.
         Console.WriteLine("-> Creating r1");
-        var r1 = new Rectangle("First Rect", 10, 10, 50, 50);
+        var r1 = new MeRectangle("First Rect", 10, 10, 50, 50);
         // Now assign a new Rectangle to r1.
         Console.WriteLine("-> Assigning r2 to r1");
-        Rectangle r2 = r1;
+        MeRectangle r2 = r1;
         // Change some values of r2.
         Console.WriteLine("-> Changing values of r2");
         r2.RectInfo.InfoString = "This is new info!";
@@ -26,18 +26,18 @@ public class Shape
         // Assignment copies the reference types, but the value types get copied.
     }
 
-    struct Rectangle
+    struct MeRectangle
     {
         // The Rectangle structure contains a reference type member.
-        public readonly Shape RectInfo;
+        public readonly MeShape RectInfo;
         public readonly int RectTop;
         public readonly int RectLeft;
         public int RectBottom;
         public readonly int RectRight;
 
-        public Rectangle(string info, int top, int left, int bottom, int right)
+        public MeRectangle(string info, int top, int left, int bottom, int right)
         {
-            RectInfo = new Shape(info);
+            RectInfo = new MeShape(info);
             RectTop = top;
             RectBottom = bottom;
             RectLeft = left;
